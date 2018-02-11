31451cdc2780e83425b157f8537ba6d962ac6e0f
Angjel Spasovski <angjel.spasovski@gmail.com>
2018-02-11 14:58:02 +0100

    Clean code

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("beekeeperApp", ['ngRoute', 'firebase']);
+var app = angular.module("beekeeperApp", ['ngRoute', 'firebase']); //,'ngMap'
 
 
 /*Configuration Routs*/
--- app/modules/services/commonProp.service.js
+++ app/modules/services/commonProp.service.js
@@ -1,20 +1,25 @@
 var app = angular.module("beekeeperApp");
 
 app.service('CommonProp', ['$location', '$firebaseAuth', function($location, $firebaseAuth){
+	
+
 	var user = "";
 	var auth = $firebaseAuth();
 
 	return {
+		/* GET users */
 		getUser: function(){
 			if(user == ""){
 				user = localStorage.getItem("userEmail");
 			}
 			return user;
 		},
+		/* SET users */
 		setUser: function(value){
 			localStorage.setItem("userEmail", value);
 			user = value;
 		},
+		/* LOGOUT */
 		logoutUser: function(){
 			auth.$signOut();
 			console.log("Logged Out Succesfully");
